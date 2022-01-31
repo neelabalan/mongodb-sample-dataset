ac0a2813d6f0b60371df99ea18ffc213d1382bac
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-01-31 19:06:04 +0100

    feat: implemented authentication

--- script.sh
+++ script.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+if [ ! -z "$3" ]; then
+    if [ ! -z "$4" ]; then
+        echo "Using password authentication!"
+        #auth="--authenticationDatabase admin -u $3 -p $4"
+    fi
+fi
+
+echo $auth
+
 for coll in *; do
     if [ -d "${coll}" ] ; then
         echo "$coll"
         for file in $coll/*; do
-            mongoimport --drop --host $1 --port $2 --db "$coll" --collection "$(basename $file .json)" --file $file
+            mongoimport --drop --host $1 --port $2 --db "$coll" --collection "$(basename $file .json)" --file $file $auth
             #echo "$(basename $file .json)"
             #echo "$file"
         done
     fi
 done
-
