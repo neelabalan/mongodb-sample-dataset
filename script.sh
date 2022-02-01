5afd0f9eaed215c485f02e7ccbd94f1239281f9a
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-02-01 15:51:40 +0100

    Implemented authentication (#1)
    * feat: implemented authentication
    
    * fix: wrong line commented out
    
    * docs: script ussage

--- script.sh
+++ script.sh
@@ -1,11 +1,25 @@
+#!/bin/bash
+###########################################################
+# Ussage ( * = optional ):                                #
+# ./script <db-address> <db-port> *<username> *<password> #
+###########################################################
+
+if [ ! -z "$3" ]; then
+    if [ ! -z "$4" ]; then
+        echo "Using password authentication!"
+        auth="--authenticationDatabase admin -u $3 -p $4"
+    fi
+fi
+
+#echo $auth
+
 for coll in *; do
     if [ -d "${coll}" ] ; then
         echo "$coll"
         for file in $coll/*; do
-            mongoimport --drop --host $1 --port $2 --db "$coll" --collection "$(basename $file .json)" --file $file
+            mongoimport --drop --host $1 --port $2 --db "$coll" --collection "$(basename $file .json)" --file $file $auth
             #echo "$(basename $file .json)"
             #echo "$file"
         done
     fi
 done
-
